80b220997e4a159652b6ba077cb4142d58a005ba
pramana yuda <pramanayuda772@gmail.com>
2023-07-05 23:33:09 +0700

    reapply gtagmanager

--- resources/views/layouts/landing/master.blade.php
+++ resources/views/layouts/landing/master.blade.php
@@ -3,20 +3,6 @@
 
 <head>
 
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-2HMVYVF6MW"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'G-2HMVYVF6MW');
-
-    </script>
-
     <!-- Google Tag Manager -->
     <script>
         (function(w, d, s, l, i) {
@@ -36,6 +22,20 @@
 
     </script>
     <!-- End Google Tag Manager -->
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-2HMVYVF6MW"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-2HMVYVF6MW');
+
+    </script>
 
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
