9be43c19508c8016ad3a775f47361067934a19bb
pramana yuda <pramanayuda772@gmail.com>
2023-07-11 02:29:04 +0700

    add single post ad on blog

--- resources/views/landing/post.blade.php
+++ resources/views/landing/post.blade.php
@@ -77,6 +77,13 @@
                             </ul>
                         </div>
 
+                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6641141866403336" crossorigin="anonymous"></script>
+                        <ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-6641141866403336" data-ad-slot="4286197238"></ins>
+                        <script>
+                            (adsbygoogle = window.adsbygoogle || []).push({});
+
+                        </script>
+
                         <div class="entry-content preserveLines">{{$post->desc}}</div>
 
                         <div class="entry-footer">
